a9d1c5cbed8886fd61aa30afd92b588c46fe09aa
Alan Steremberg <alan.steremberg@gmail.com>
2017-02-26 17:56:38 -0800

    added camera calibration - create pickle if needed

--- lanefind.py
+++ lanefind.py
@@ -3,6 +3,9 @@ import numpy as np
 import cv2
 import pickle
 from collections import deque
+import os.path
+import glob
+
 
 
 class Line():
@@ -81,6 +84,52 @@ class Line():
         self.radius_of_curvature = curverad
         return
 
+
+def createCameraDistortionPickle(pickle_file_name):
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d points in real world space
+    imgpoints = [] # 2d points in image plane.
+
+    # Make a list of calibration images
+    images = glob.glob('camera_cal/calibration*.jpg')
+
+    # Step through the list and search for chessboard corners
+    for idx, fname in enumerate(images):
+        print(fname)
+        img = cv2.imread(fname)
+        img_size = (img.shape[1], img.shape[0])
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+
+        # If found, add object points, image points
+        if ret == True:
+            #print(fname+' found')
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+        else:
+            print(fname+' not found')
+
+    # Do camera calibration given object points and image points
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+
+
+    if not os.path.exists('camera_cal_output'):
+        os.makedirs('camera_cal_output')
+
+    # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+    dist_pickle = {}
+    dist_pickle["mtx"] = mtx
+    dist_pickle["dist"] = dist
+    pickle.dump( dist_pickle, open( "camera_cal_output/dist_pickle.p", "wb" ) )
+
 #
 #  This function for our pipeline removes the camera distortion
 #  we need to load the camera information if it isn't loaded already
@@ -91,6 +140,9 @@ def correctCameraDistortion(image):
     global dist2_pickle
     if not dist2_pickle:
         # load the pickle
+        pickle_file_name='camera_cal_output/dist_pickle.p'
+        if (not os.path.isfile(pickle_file_name)):
+            createCameraDistortionPickle(pickle_file_name)
         dist2_pickle=pickle.load(open( 'camera_cal_output/dist_pickle.p', "rb" ) )
     mtx=dist2_pickle["mtx"]
     dist=dist2_pickle["dist"]
@@ -311,6 +363,7 @@ def lanePipeline(image):
     # find where we are in the lane
     position = findPositionInLane((img.shape[1]/2),left_fitx,right_fitx)
 
+    # let's skip when they aren't parallel -- we should probably only do this for 5 frames (TODO)
     if (left_line.best_fit is None or right_line.best_fit is None or line_is_parallel):
        left_line.validate_and_update(leftx,lefty,left_fitx,left_curverad,position)
        right_line.validate_and_update(rightx,righty,right_fitx,right_curverad,position)
@@ -319,56 +372,6 @@ def lanePipeline(image):
     # draw the lines onto the image
     #
     result=drawOutput(orig_img,left_line.best_fit,right_line.best_fit,ploty)
-
-    #def validate_and_update(self,fitx,curverad):
-    # check to see if the left and right lines are valid
-    #left_line_valid = True
-    #if (left_line is not None):
-    #     if not left_line.validate(left_fitx,left_curverad):
-    #        left_line_valid = False
-    #else:
-    #    left_line = Line()
-    #right_line_valid = True
-    #if (right_line is not None):
-    #     if not right_line.validate(right_fitx,right_curverad):
-    #        right_line_valid = False
-    #else:
-    #    right_line = Line()
-    # 
-    #if left_line_valid:
-    #     cur_left_line.detected = True
-    #     cur_left_line.radius_of_curvature= left_curverad
-    #     cur_left_line.line_base_pos = left_fitx[719] 
-    #    cur_left_line.current_fit = left_fitx
-    #    cur_left_line.recent_xfitted.append(left_fitx)
-    #    if (len(cur_left_line.recent_xfitted)>5):
-    #       cur_left_line.recent_xfitted.pop()
-
-    #if right_line_valid:
-    #    cur_right_line.detected = True
-    #    cur_right_line.radius_of_curvature= right_curverad
-    #    cur_right_line.line_base_pos = right_fitx[719] 
-    #    cur_right_line.current_fit = right_fitx
-    #    cur_right_line.recent_xfitted.append(right_fitx)
-    #    if (len(cur_right_line.recent_xfitted)>5):
-    #       cur_right_line.recent_xfitted.pop()
-        #average x values of the fitted line over the last n iterations
-        #self.bestx = None     
-        #polynomial coefficients averaged over the last n iterations
-        #self.best_fit = None  
-        #polynomial coefficients for the most recent fit
-        #self.current_fit = [np.array([False])]  
-        #distance in meters of vehicle center from the line
-        #self.line_base_pos = None 
-        #difference in fit coefficients between last and new fits
-        #self.diffs = np.array([0,0,0], dtype='float') 
-        #x values for detected line pixels
-        #self.allx = None  
-        #y values for detected line pixels
-        #self.ally = None
-
-
-
 
     #
     #  draw the radius and position on image
